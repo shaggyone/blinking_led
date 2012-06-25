e93695ee9585966754c50aa20824a8602543f6e0
Victor Zagorski <victor@zagorski.ru>
2012-06-25 11:59:05 +0800

    Removed comments.

--- led_blink.c
+++ led_blink.c
@@ -1,13 +1,7 @@
-/* all indications between diagonal stroke star and star diagonal stroke are only
-comments */
-// line comments are likewise possible
-// all indications of a line following on the two diagonal strokes are comment
-// #include <stdint.h>
-// #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main (void) { // (2)
+int main (void) {
   DDRB = 0x01;
 
   while(1) {
@@ -16,7 +10,7 @@ int main (void) { // (2)
     PORTB = 0;
     _delay_ms(500);
   }
-  /* never reached */
+
   return 0;
 }
 
