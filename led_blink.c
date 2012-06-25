620b6deefd658d0593a48c6976fe198f59e66d1e
Victor Zagorski <victor@zagorski.ru>
2012-06-25 11:30:37 +0800

    Example, not working fow some reason

--- led_blink.c
+++ led_blink.c
@@ -3,35 +3,21 @@ comments */
 // line comments are likewise possible
 // all indications of a line following on the two diagonal strokes are comment
 // #include <stdint.h>
-//#define F_CPU 8000000UL
-#include <avr/io.h> // (1)
+// #define F_CPU 8000000UL
+#include <avr/io.h>
 #include <util/delay.h>
 
 int main (void) { // (2)
   int i, j;
 
-  DDRC = 0x00;
   DDRB = 0x01;//PORTB = 0x03;
   while(1) {
-    PORTB = PORTC;
-
-
-/*  for (j=1; j < 20000; j = j * 2) {
-      PORTB = 1;
-      for (i=0; i< j; i++)
-        _delay_ms(1);
-      PORTB = 0;
-      for (i=0; i< j; i++) ;
-      _delay_ms(1);
-    }*/
+    PORTB = 1;
+    _delay_ms(500);
+    PORTB = 0;
+    _delay_ms(500);
   }
   /* never reached */
   return 0;
-// (3)equal to DDRB=0b11111111
-// (4)equal to PORTB=0b00000011
-// (5a)
-// (5b)
-// (5c)
-// (6)
 }
 
